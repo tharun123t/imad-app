31a145648b871e37772910ab62a01117b2551d26
tharun123t <30842510+tharun123t@users.noreply.github.com>
2017-08-17 21:28:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+
+    app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
